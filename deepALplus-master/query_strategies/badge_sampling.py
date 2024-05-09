d4a7203ff9b36c67d41cfb92e2eefdd262c4da0a
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2024-05-08 19:05:49 -0500

    Update badge_sampling.py

--- deepALplus-master/query_strategies/badge_sampling.py
+++ deepALplus-master/query_strategies/badge_sampling.py
@@ -1,62 +1,202 @@
+# import numpy as np
+# import torch
+# from .strategy import Strategy
+# from scipy import stats
+# from sklearn.metrics import pairwise_distances
+# import pdb
+
+# '''
+# This implementation is originated from https://github.com/JordanAsh/badge.
+# Please cite the original paper if you use this method.
+# @inproceedings{ash2019deep,
+#   author    = {Jordan T. Ash and
+#                Chicheng Zhang and
+#                Akshay Krishnamurthy and
+#                John Langford and
+#                Alekh Agarwal},
+#   title     = {Deep Batch Active Learning by Diverse, Uncertain Gradient Lower Bounds},
+#   booktitle = {8th International Conference on Learning Representations, {ICLR} 2020,
+#                Addis Ababa, Ethiopia, April 26-30, 2020},
+#   publisher = {OpenReview.net},
+#   year      = {2020}
+# }
+# '''
+
+# class BadgeSampling(Strategy):
+#     def __init__(self, dataset, net, args_input, args_task):
+#         super(BadgeSampling, self).__init__(dataset, net, args_input, args_task)
+
+#     def query(self, n):
+#         unlabeled_idxs, unlabeled_data = self.dataset.get_unlabeled_data()
+#         gradEmbedding = self.get_grad_embeddings(unlabeled_data)
+#         chosen = init_centers(gradEmbedding, n)
+#         return unlabeled_idxs[chosen]
+
+# # kmeans ++ initialization
+# def init_centers(X, K):
+#     ind = np.argmax([np.linalg.norm(s, 2) for s in X])
+#     mu = [X[ind]]
+#     indsAll = [ind]
+#     centInds = [0.] * len(X)
+#     cent = 0
+#     # print('#Samps\tTotal Distance')
+#     while len(mu) < K:
+#         if len(mu) == 1:
+#             D2 = pairwise_distances(X, mu).ravel().astype(float)
+#         else:
+#             newD = pairwise_distances(X, [mu[-1]]).ravel().astype(float)
+#             for i in range(len(X)):
+#                 if D2[i] >  newD[i]:
+#                     centInds[i] = cent
+#                     D2[i] = newD[i]
+#         # print(str(len(mu)) + '\t' + str(sum(D2)), flush=True)
+#         if sum(D2) == 0.0: pdb.set_trace()
+#         D2 = D2.ravel().astype(float)
+#         Ddist = (D2 ** 2)/ sum(D2 ** 2)
+#         customDist = stats.rv_discrete(name='custm', values=(np.arange(len(D2)), Ddist))
+#         ind = customDist.rvs(size=1)[0]
+#         while ind in indsAll: ind = customDist.rvs(size=1)[0]
+#         mu.append(X[ind])
+#         indsAll.append(ind)
+#         cent += 1
+#     return indsAll
 import numpy as np
-import torch
+from torch.utils.data import DataLoader
 from .strategy import Strategy
-from scipy import stats
-from sklearn.metrics import pairwise_distances
+import pickle
+from scipy.spatial.distance import cosine
+import sys
+import gc
+from scipy.linalg import det
+from scipy.linalg import pinv as inv
+from copy import copy as copy
+from copy import deepcopy as deepcopy
+import torch
+from torch import nn
+import torchfile
+from torch.autograd import Variable
+import resnet
+import vgg
+import torch.optim as optim
 import pdb
-
-'''
-This implementation is originated from https://github.com/JordanAsh/badge.
-Please cite the original paper if you use this method.
-@inproceedings{ash2019deep,
-  author    = {Jordan T. Ash and
-               Chicheng Zhang and
-               Akshay Krishnamurthy and
-               John Langford and
-               Alekh Agarwal},
-  title     = {Deep Batch Active Learning by Diverse, Uncertain Gradient Lower Bounds},
-  booktitle = {8th International Conference on Learning Representations, {ICLR} 2020,
-               Addis Ababa, Ethiopia, April 26-30, 2020},
-  publisher = {OpenReview.net},
-  year      = {2020}
-}
-'''
-
-class BadgeSampling(Strategy):
-    def __init__(self, dataset, net, args_input, args_task):
-        super(BadgeSampling, self).__init__(dataset, net, args_input, args_task)
-
-    def query(self, n):
-        unlabeled_idxs, unlabeled_data = self.dataset.get_unlabeled_data()
-        gradEmbedding = self.get_grad_embeddings(unlabeled_data)
-        chosen = init_centers(gradEmbedding, n)
-        return unlabeled_idxs[chosen]
+from torch.nn import functional as F
+import argparse
+import torch.nn as nn
+from collections import OrderedDict
+from scipy import stats
+import numpy as np
+import scipy.sparse as sp
+from itertools import product
+from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import pairwise_distances_argmin_min
+from sklearn.utils.extmath import row_norms, squared_norm, stable_cumsum
+from sklearn.utils.sparsefuncs_fast import assign_rows_csr
+from sklearn.utils.sparsefuncs import mean_variance_axis
+from sklearn.utils.validation import _num_samples
+from sklearn.utils import check_array
+from sklearn.utils import gen_batches
+from sklearn.utils import check_random_state
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import FLOAT_DTYPES
+from sklearn.metrics.pairwise import rbf_kernel as rbf
+#from sklearn.externals.six import string_types
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.metrics import pairwise_distances
 
 # kmeans ++ initialization
 def init_centers(X, K):
-    ind = np.argmax([np.linalg.norm(s, 2) for s in X])
-    mu = [X[ind]]
+    embs = torch.Tensor(X)
+    ind = torch.argmax(torch.norm(embs, 2, 1)).item()
+    embs = embs.cuda()
+    mu = [embs[ind]]
     indsAll = [ind]
-    centInds = [0.] * len(X)
+    centInds = [0.] * len(embs)
     cent = 0
-    # print('#Samps\tTotal Distance')
+    print('#Samps\tTotal Distance')
     while len(mu) < K:
         if len(mu) == 1:
-            D2 = pairwise_distances(X, mu).ravel().astype(float)
+            D2 = torch.cdist(mu[-1].view(1,-1), embs, 2)[0].cpu().numpy()
         else:
-            newD = pairwise_distances(X, [mu[-1]]).ravel().astype(float)
-            for i in range(len(X)):
+            newD = torch.cdist(mu[-1].view(1,-1), embs, 2)[0].cpu().numpy()
+            for i in range(len(embs)):
                 if D2[i] >  newD[i]:
                     centInds[i] = cent
                     D2[i] = newD[i]
-        # print(str(len(mu)) + '\t' + str(sum(D2)), flush=True)
+        print(str(len(mu)) + '\t' + str(sum(D2)), flush=True)
         if sum(D2) == 0.0: pdb.set_trace()
         D2 = D2.ravel().astype(float)
         Ddist = (D2 ** 2)/ sum(D2 ** 2)
         customDist = stats.rv_discrete(name='custm', values=(np.arange(len(D2)), Ddist))
         ind = customDist.rvs(size=1)[0]
         while ind in indsAll: ind = customDist.rvs(size=1)[0]
-        mu.append(X[ind])
+        mu.append(embs[ind])
         indsAll.append(ind)
         cent += 1
     return indsAll
+
+def distance(X1, X2, mu):
+    Y1, Y2 = mu
+    X1_vec, X1_norm_square = X1
+    X2_vec, X2_norm_square = X2
+    Y1_vec, Y1_norm_square = Y1
+    Y2_vec, Y2_norm_square = Y2
+    dist = X1_norm_square * X2_norm_square + Y1_norm_square * Y2_norm_square - 2 * (X1_vec @ Y1_vec) * (X2_vec @ Y2_vec)
+    # Numerical errors may cause the distance squared to be negative.
+    assert np.min(dist) / np.max(dist) > -1e-4
+    dist = np.sqrt(np.clip(dist, a_min=0, a_max=None))
+    return dist
+
+def init_centers(X1, X2, chosen, chosen_list,  mu, D2):
+    if len(chosen) == 0:
+        ind = np.argmax(X1[1] * X2[1])
+        mu = [((X1[0][ind], X1[1][ind]), (X2[0][ind], X2[1][ind]))]
+        D2 = distance(X1, X2, mu[0]).ravel().astype(float)
+        D2[ind] = 0
+    else:
+        newD = distance(X1, X2, mu[-1]).ravel().astype(float)
+        D2 = np.minimum(D2, newD)
+        D2[chosen_list] = 0
+        Ddist = (D2 ** 2) / sum(D2 ** 2)
+        customDist = stats.rv_discrete(name='custm', values=(np.arange(len(Ddist)), Ddist))
+        ind = customDist.rvs(size=1)[0]
+        while ind in chosen: ind = customDist.rvs(size=1)[0]
+        mu.append(((X1[0][ind], X1[1][ind]), (X2[0][ind], X2[1][ind])))
+    chosen.add(ind)
+    chosen_list.append(ind)
+    print(str(len(mu)) + '\t' + str(sum(D2)), flush=True)
+    return chosen, chosen_list, mu, D2
+
+class BadgeSampling(Strategy):
+    def __init__(self, X, Y, idxs_lb, net, handler, args):
+        super(BadgeSampling, self).__init__(X, Y, idxs_lb, net, handler, args)
+
+    def query(self, n):
+        idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
+        gradEmbedding = self.get_grad_embedding(self.X[idxs_unlabeled], self.Y.numpy()[idxs_unlabeled]).numpy()
+        chosen = init_centers(gradEmbedding, n)
+        return idxs_unlabeled[chosen]
+
+
+    def query(self, n):
+        idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
+        embs, probs = self.get_embedding(self.X[idxs_unlabeled], self.Y.numpy()[idxs_unlabeled], return_probs=True)
+        embs = embs.numpy()
+        probs = probs.numpy()
+
+        # the logic below reflects a speedup proposed by Zhang et al.
+        # see Appendix D of https://arxiv.org/abs/2306.09910 for more details
+        m = (~self.idxs_lb).sum()
+        mu = None
+        D2 = None
+        chosen = set()
+        chosen_list = []
+        emb_norms_square = np.sum(embs ** 2, axis=-1)
+        max_inds = np.argmax(probs, axis=-1)
+
+        probs = -1 * probs
+        probs[np.arange(m), max_inds] += 1
+        prob_norms_square = np.sum(probs ** 2, axis=-1)
+        for _ in range(n):
+            chosen, chosen_list, mu, D2 = init_centers((probs, prob_norms_square), (embs, emb_norms_square), chosen, chosen_list, mu, D2)
+        return idxs_unlabeled[chosen_list]
