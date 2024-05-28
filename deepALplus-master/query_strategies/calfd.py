2ac2551aac6a1007c0682f1583ec3ab646ec99a5
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2024-05-27 22:47:47 -0500

    Update calfd.py

--- deepALplus-master/query_strategies/calfd.py
+++ deepALplus-master/query_strategies/calfd.py
@@ -85,7 +85,7 @@ class CALFD(Strategy):
         knei_dist.append(np.mean(curr_dist[j, :num_nei]))
       sortIndex1 = np.argsort(query_priority)
       sortIndex1 = sortIndex1[::-1]
-      dth = 0.01* np.mean(knei_dist)
+      dth = 0.001* np.mean(knei_dist)
       query_priority = np.array(query_priority)
       fet1 = self.diversityfetch1(sortIndex1[:round(len(query_priority) / 2)],
                                   curr_cluster,
