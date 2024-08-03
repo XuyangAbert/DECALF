ca256a1c0ca927d05cdbc863345655de830040f6
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2024-08-02 21:47:33 -0500

    Update calfd.py

--- deepALplus-master/query_strategies/calfd.py
+++ deepALplus-master/query_strategies/calfd.py
@@ -111,10 +111,12 @@ class CALFD(Strategy):
         # temp_d3 = np.linalg.norm(samples[curr_cluster[fil_index[k]], :] - temp_neigh3)
         # temp_ratio1 = max(temp_d1, temp_d2) / min(temp_d1, temp_d2)
         # d2.append(temp_ratio1)
-      d2 = np.array(d2)
-      fet2 = self.diversityfetch2(fil_index, curr_cluster,
-                                  d2, curr_dist, dth,
-                                  round(num_queries * 0.5))
+      # d2 = np.array(d2)
+      sortIndex2 = np.argsort(d2)
+      fet2 = fil_index[sortIndex2[-round(num_queries * 0.5):]]
+      # fet2 = self.diversityfetch2(fil_index, curr_cluster,
+      #                             d2, curr_dist, dth,
+      #                             round(num_queries * 0.5))
       # sortIndex2 = np.argsort(d2)
       # candidate_fet2 = fil_index[sortIndex2[:int(round(num_queries * 1))]] # 0.8
       # candidate_fet2 = fil_index
