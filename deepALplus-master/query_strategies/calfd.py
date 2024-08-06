1addb6e3d5cbda6259e8bcf1b778dfbfe69706e5
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2024-08-06 15:37:14 -0500

    Update calfd.py

--- deepALplus-master/query_strategies/calfd.py
+++ deepALplus-master/query_strategies/calfd.py
@@ -142,16 +142,16 @@ class CALFD(Strategy):
     unlabeled_idxs, unlabeled_data = self.dataset.get_unlabeled_data()
     embedding_unlabeled = self.get_embeddings(unlabeled_data).numpy()
     # unlabeled_raw = self.get_raw_embeddings(unlabeled_data)
-    num_clusters = 500
-    if num_clusters <= 50:
-        km = KMeans(n_clusters=num_clusters)
-        km.fit_predict(embedding_unlabeled)
-    else:
-        km = MiniBatchKMeans(n_clusters=num_clusters, batch_size=5000)
-        km.fit_predict(embedding_unlabeled)
-    cluster_centers, cluster_idx = km.cluster_centers_, km.labels_
-    # clustering_model = fps_analysis()
-    # cluster_centers, cluster_idx, cluster_dist = clustering_model.predict(embedding_unlabeled)
+    # num_clusters = 500
+    # if num_clusters <= 50:
+    #     km = KMeans(n_clusters=num_clusters)
+    #     km.fit_predict(embedding_unlabeled)
+    # else:
+    #     km = MiniBatchKMeans(n_clusters=num_clusters, batch_size=5000)
+    #     km.fit_predict(embedding_unlabeled)
+    # cluster_centers, cluster_idx = km.cluster_centers_, km.labels_
+    clustering_model = fps_analysis()
+    cluster_centers, cluster_idx, cluster_dist = clustering_model.predict(embedding_unlabeled)
     print("clustering stage finish!")
     query_idx = self.active_query(embedding_unlabeled, cluster_centers, cluster_idx, label_budget)
     query_idx = query_idx.astype(int)
