9e0dc7f9c5ffda030f9ee80a0efed7a2c6584669
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2024-08-10 21:27:38 -0500

    Update decalf.py

--- deepALplus-master/query_strategies/decalf.py
+++ deepALplus-master/query_strategies/decalf.py
@@ -122,7 +122,7 @@ class DECALF(Strategy):
       sortIndex2 = np.argsort(d2)
       # candidate_fet2 = fil_index[sortIndex2[:int(round(num_queries * 0.8))]]
       # fet2 = curr_cluster[fil_index[sortIndex2[:round(num_queries * 0.5)]]] # fil_index[sortIndex2]
-      fet2 = self.diversityfetch2(candidate_fet2 , curr_cluster,
+      fet2 = self.diversityfetch2(fil_index, curr_cluster,
                                   d2, curr_dist, dth,
                                   math.ceil(num_queries * 0.5))
       # fil_index = sortIndex1[-int(round(len(query_priority) / 2)):]
